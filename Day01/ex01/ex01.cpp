58d5cc2f3b3dcfadcf107740496200ae65a05826
Etheram68 <francois681927@hotmail.fr>
2020-07-07 10:37:02 +0200

    Add delete on file

---
+++ Day01/ex01/ex01.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ex01.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frfrey <frfrey@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/07 10:35:44 by frfrey            #+#    #+#             */
+/*   Updated: 2020/07/07 10:35:44 by frfrey           ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+void memoryLeak()
+{
+	std::string* panthere = new std::string("String panthere");
+	std::cout << *panthere << std::endl;
+	delete panthere;
+}
